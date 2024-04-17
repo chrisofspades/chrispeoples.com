5ea766c239f65d1021b990743308d831c9a531ac
chrisofspades <chrisofspades@gmail.com>
2024-04-16 19:32:06 -0700

    Fix time localize script

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,7 @@
 function localizeTimeElements(e) {
-    let times = document.querySelectorAll("time");
+    let times = document.querySelectorAll("time[data-localize]");
     times.forEach(time => {
-        let date = new Date(time.getAttribute("datetime"));
+        let date = new Date(time.getAttribute("datetime") || time.innerText);
         time.innerText = date.toLocaleString("en-CA", { dateStyle: "short", timeStyle: "short"});
     })
 }
